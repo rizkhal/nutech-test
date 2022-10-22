ab672d1f5fd77e5ef7c0e4efdc6376d975d4ded0
rizkhal <lamaaurizkhal@gmail.com>
2022-10-22 12:23:12 +0800

    fix: redirect

--- routes/web/index.php
+++ routes/web/index.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn () => redirect('/products'));
+
 Route::resource('products', ProductController::class)->except(['create', 'edit', 'update']);
 // inertia uploading file must be using post
 Route::post('products/update/{product}', [ProductController::class, 'update'])->name('products.update');
